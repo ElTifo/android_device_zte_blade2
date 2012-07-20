fdf9320a6db223494a88caee728a1ccb6d8dd5de
Daz Jones <yuki@thebronasium.com>
2012-07-20 11:20:25 +0100

    Test new key handling

--- recovery_ui.c
+++ recovery_ui.c
@@ -34,25 +34,34 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
 int device_handle_key(int key_code, int visible) {
     if (visible) {
         switch (key_code) {
+            case KEY_CAPSLOCK:
             case KEY_DOWN:
             case KEY_VOLUMEUP:
                 return HIGHLIGHT_UP;
 
+            case KEY_LEFTSHIFT:
             case KEY_UP:
             case KEY_VOLUMEDOWN:
                 return HIGHLIGHT_DOWN;
 
+            case KEY_LEFTBRACE:
+            case KEY_ENTER:
+            case BTN_MOUSE:
+            case KEY_CENTER:
+            case KEY_CAMERA:
+            case KEY_F21:
+            case KEY_SEND:
             case KEY_POWER:
-                    return SELECT_ITEM;
-
-	    case KEY_HOME:
+            case 62:
+            case KEY_HOME:
                 return SELECT_ITEM;
 
+            case KEY_END:
+            case KEY_BACKSPACE:
             case KEY_BACK:
                 if (!get_allow_toggle_display())
                     return GO_BACK;
         }
     }
-
     return NO_ACTION;
 }
