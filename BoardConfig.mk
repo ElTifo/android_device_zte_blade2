f1aa11924fe0cefd8c309ca35825604e5058198c
ElTifo <tifo.el@gmail.com>
2014-02-02 22:25:42 +0000

    Update BoardConfig.mk
    Added target arch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,11 +1,17 @@
 USE_CAMERA_STUB := true
 
+# CPU
+TARGET_ARCH := arm
+TARGET_CPU_ABI := armeabi
+TARGET_CPU_ABI2 := armeabi-v6l
+TARGET_CPU_VARIANT := arm11
+TARGET_ARCH_VARIANT := armv6-vfp
+
 # inherit from the proprietary version
 -include vendor/zte/blade2/BoardConfigVendor.mk
 
 TARGET_NO_BOOTLOADER := true
 TARGET_BOARD_PLATFORM := unknown
-TARGET_CPU_ABI := armeabi
 TARGET_BOOTLOADER_BOARD_NAME := blade2
 
 BOARD_KERNEL_CMDLINE := 
